b107e5711429ca505de3a56e28fc35c3f993dec1
Jeff Cohen <cohen.jeff@gmail.com>
2016-04-29 15:59:26 -0500

    Reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,24 @@ Rails.application.routes.draw do
 
   root 'movies#index'
 
-  get '/directors' => 'directors#index'
-  get '/directors/:id' => 'directors#show'
-  get '/directors/new' => 'directors#new'
-  post '/directors' => 'directors#create'
-  get '/directors/:id/edit' => 'directors#edit'
-  patch '/directors/:id/update' => 'directors#edit'
-  delete '/directors/:id' => 'directors#destroy'
-
-  get '/actors' => 'actors#index'
-  get '/actors/:id' => 'actors#show'
-
-  get '/movies/new' => 'movies#new'
-  post '/movies' => 'movies#create'
   get '/movies' => 'movies#index'
+  post '/movies' => 'movies#create'
+  get '/movies/new' => 'movies#new'
   get '/movies/:id' => 'movies#show'
   delete '/movies/:id' => 'movies#destroy'
   patch '/movies/:id' => 'movies#update'
   get '/movies/:id/edit' => 'movies#edit'
+
+  get '/directors' => 'directors#index'
+  post '/directors' => 'directors#create'
+  get '/directors/new' => 'directors#new'
+  get '/directors/:id' => 'directors#show'
+  patch '/directors/:id' => 'directors#edit'
+  delete '/directors/:id' => 'directors#destroy'
+  get '/directors/:id/edit' => 'directors#edit'
+
+  get '/actors' => 'actors#index'
+  get '/actors/:id' => 'actors#show'
+
 
 end
